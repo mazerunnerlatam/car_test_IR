103bdad2a046286e7056c24041a3470b26010737
mazerunnerlatam <mazerunner.latam@gmail.com>
2025-05-02 22:09:35 -0300

    auto rojo 3d printed

--- car_test_IR.ino
+++ car_test_IR.ino
@@ -5,11 +5,12 @@ IR diode connected to analog input:A3
 */
 
 // Analog as Digital
-#define ir1 14
-#define ir2 15
+#define ir1 14 // This is A0
+#define ir2 15 // This is A1
 #define ir3 16
 #define ir4 17
 #define ir5 18
+#define ir6 19 // Thhis is A5
 
 void setup() {
   Serial.begin(9600);
@@ -20,25 +21,42 @@ void setup() {
   pinMode(ir3, INPUT);
   pinMode(ir4, INPUT);
   pinMode(ir5, INPUT);
+
+  Serial.println("About to start...");
+  delay (1000);
 }
 
 void loop() {
-  int slm = analogRead(ir1);
-  int sl = digitalRead(ir2); // Please Notice this is a DigitalREAD
-  int sc = analogRead(A2);
+  /*
+  int slmm = analogRead(ir1);
+  int slm = analogRead(ir2); // Please Notice this is a DigitalREAD
+  int sl = analogRead(A2);
   int sr = analogRead(A3);
   int srm = analogRead(A4);
+  int srmm = analogRead(A5); // Sensor Right More More in config 6 IRs
+  */
 
+  int slmm = !digitalRead(ir1); // Symbol !  means NOT
+  int slm = digitalRead(ir2); // Please Notice this is a DigitalREAD
+  int sl = digitalRead(A2);
+  int sr = digitalRead(A3);
+  int srm = digitalRead(A4);
+  int srmm = digitalRead(A5); // Sensor Right More More in config 6 IRs
+
+
+
+  Serial.print(slmm);
+  Serial.print("\t");
   Serial.print(slm);
   Serial.print("\t");
   Serial.print(sl);
   Serial.print("\t");
-  Serial.print(sc);
-  Serial.print("\t");
   Serial.print(sr);
   Serial.print("\t");
   Serial.print(srm);
-  Serial.println("\t => Notice 2nd Sensor has a DigitalREAD: 1 or 0");
+  Serial.print("\t");
+  Serial.print(srm);
+  Serial.println("\t => Notice one sensor has a DigitalREAD: 1 or 0");
   
 
   delay(500); 
